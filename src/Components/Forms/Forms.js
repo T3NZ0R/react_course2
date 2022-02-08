c5587b5fb618c40bbca6289db344cf3aab19c594
T3NZ0R <t3nz000r@gmail.com>
2022-02-08 20:26:08 +0200

    cw3

--- src/Components/Forms/Forms.js
+++ src/Components/Forms/Forms.js
@@ -6,16 +6,24 @@ const Forms = ({dispatch}) => {
     const [cat, setCat] = useState({name:''});
 
     const formHandler = (e) => {
-        const eventInfo = {...cat, [e.target.name]: e.target.value}
+        const eventInfo = {...cat, [e.target.name]: e.target.value};
+        console.log(cat);
         setCat({...cat, ...eventInfo})
     }
+
+    const getFormInfo = (e) => {
+        e.preventDefault()
+        console.log(cat);
+        dispatch({type: 'cat', payload: cat})
+    }
+
 
     return (
         <div>
 
             <form>
                 <label>Name: <input type="text" name={"cat"} value={cat.name} onChange={formHandler}/></label>
-                <button onClick={() => dispatch({type: 'cat', payload: cat})}>Add</button>
+                <button onClick={getFormInfo}>Add</button>
             </form>
 
         </div>
